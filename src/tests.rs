8e80365d996f04d7e9c144139a48cb134b1eb72c
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-12-21 14:48:25 +0100

    Fix test names

--- src/tests.rs
+++ src/tests.rs
@@ -8,21 +8,21 @@ fn test_split_group_in_teams() {
 }
 
 #[test]
-fn test_compute_team_size_when_divisible() {
+fn test_compute_team_sizes_when_divisible() {
     assert_eq!(compute_team_sizes(16, 4), vec![4, 4, 4, 4]);
 }
 
 #[test]
-fn test_compute_team_size_when_one_less() {
+fn test_compute_team_sizes_when_one_less() {
     assert_eq!(compute_team_sizes(15, 4), vec![4, 4, 4, 3]);
 }
 
 #[test]
-fn test_compute_team_size_when_one_more() {
+fn test_compute_team_sizes_when_one_more() {
     assert_eq!(compute_team_sizes(17, 4), vec![4, 4, 4, 5]);
 }
 
 #[test]
-fn test_compute_team_size_adjusting_two() {
+fn test_compute_team_sizes_adjusting_last_two() {
     assert_eq!(compute_team_sizes(18, 4), vec![4, 4, 4, 3, 3]);
 }
