f988b4b4b7dd6f4528a35c3aeba420603f6d0801
tristan <tristacha59@gmail.com>
2023-01-10 12:09:48 +0100

    feat: basic route with sources name files

---
+++ webapp_project/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from webapp_project.utils import get_file_name_from_name_providers
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(directory="templates")
+
+# input du fichier
+# choix de la taille des équipes
+# choix du type de nom des équipes
+# offset ?
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    sources = get_file_name_from_name_providers()
+    context = {"request": request, "sources": sources}
+    return templates.TemplateResponse("index.html", context)
