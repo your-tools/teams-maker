0aed303c703528937602934f3fb20b2808c18627
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-11-02 21:11:20 +0100

    Fix some typos

--- teams_maker/cli.py
+++ teams_maker/cli.py
@@ -6,13 +6,13 @@ from teams_maker.teams import create_teams, get_team_name, check_name_provider
 
 def main() -> None:
     parser = ArgumentParser(prog="teams-maker")
-    parser.add_argument("partipant_list", type=Path)
+    parser.add_argument("participant_list", type=Path)
     parser.add_argument("--team-size", type=int, required=True)
     parser.add_argument("--name-provider")
     parser.add_argument("--offset", type=int, default=0)
     args = parser.parse_args()
 
-    list_path = args.partipant_list
+    list_path = args.participant_list
     team_size = args.team_size
     name_provider = args.name_provider
     offset = args.offset
@@ -25,8 +25,8 @@ def main() -> None:
             name_provider=name_provider, index=index, offset=offset
         )
         print("-" * 10, "Team", team_name, "-" * 10)
-        for partipant in sorted(team):
-            print(partipant)
+        for participant in sorted(team):
+            print(participant)
         print()
 
 
